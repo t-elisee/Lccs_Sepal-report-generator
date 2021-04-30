fd33be7757b166c11d04edb32bd89a46f3464090
Elisée Tchana <37068938+t-elisee@users.noreply.github.com>
2021-04-30 14:05:20 +0200

    Delete lccs_sepal_pdf directory

--- lccs_sepal_pdf/app.js
+++
@@ -1,8 +0,0 @@
-var element = document.getElementById('element');
-html2pdf(element, {
-    margin:       10,
-    filename:     'myfile.pdf',
-
-    html2canvas:  { scale: 2, logging: true, dpi: 192, letterRendering: true },
-    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
-  });
